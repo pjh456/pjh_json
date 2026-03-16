81bb99ffeb549263cb2f347499663991bbde5e35
pjh456 <147148383@qq.com>
2026-03-16 15:31:36 +0800

    test: 添加了 Json 类型接口测试

---
+++ tests/json_types.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <cassert>
+#include <pjh_json/json.hpp>
+
+using namespace pjh::json;
+
+void simple_value()
+{
+    std::cout << "Json Value test started." << std::endl;
+
+    Json null_val(nullptr);
+    assert(null_val.is_null());
+
+    Json bool_val(true);
+    assert(bool_val.is_boolean());
+    assert(bool_val.as_boolean());
+    assert(bool_val == true);
+
+    Json int_val((int64_t)12);
+    assert(int_val.is_int());
+    assert(int_val.as_int() == (int64_t)12);
+    assert(int_val == (int64_t)12);
+
+    Json float_val((double)1.2);
+    assert(float_val.is_float());
+    assert(float_val.as_float() == (double)1.2);
+    assert(float_val == (double)1.2);
+
+    Json str_val("str");
+    assert(str_val.is_string());
+    assert(str_val.as_string() == "str");
+    assert(str_val == "str");
+
+    std::cout << "Json Value test passed." << std::endl;
+}
+
+void array_value()
+{
+    std::cout << "Json Array test started." << std::endl;
+
+    auto arr1 = std::move(make_array({}));
+    assert(arr1.empty());
+    assert(arr1.is_array());
+    assert(arr1.size() == 0);
+    try
+    {
+        auto val = arr1.at(0);
+        assert(false);
+    }
+    catch (...)
+    {
+    }
+
+    auto arr2 = std::move(make_array({make_str("pjh"), make_int(123)}));
+    assert(!arr2.empty());
+    assert(arr1.is_array());
+    assert(arr2.size() == 2);
+    assert(arr2[0] == "pjh" && arr2[1] == (int64_t)123);
+
+    std::cout << "Json Array test passed." << std::endl;
+}
+
+void object_value()
+{
+    std::cout << "Json Object test started." << std::endl;
+
+    auto obj1 = std::move(make_object({}));
+    assert(obj1.empty());
+    assert(obj1.is_object());
+    assert(obj1.size() == 0);
+
+    auto obj2 = std::move(make_object({{"pjh", make_int(123)}, {"123", make_str("pjh")}}));
+    assert(!obj2.empty());
+    assert(obj2.is_object());
+    assert(obj2.size() == 2);
+    assert(obj2["pjh"] == (int64_t)123 && obj2["123"] == "pjh");
+
+    std::cout << "Json Object test passed." << std::endl;
+}
+
+int main()
+{
+    simple_value();
+    array_value();
+    object_value();
+}
