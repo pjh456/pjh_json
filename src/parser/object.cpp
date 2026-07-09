0b4f2001ef334c0c665e511472632448f39c5f89
pjh456 <147148383@qq.com>
2026-07-09 17:44:37 +0800

    refactor: 复用代码以减少冗余实现

--- src/parser/object.cpp
+++ src/parser/object.cpp
@@ -14,48 +14,9 @@ namespace pjh::json
 
     Json Parser::parse_object()
     {
-        ++m_curr;
-        Object obj(m_resource);
-        skip_whitespace();
-        if (*m_curr == '}')
-        {
-            ++m_curr;
-            return Json(std::move(obj));
-        }
-
-        obj.data().reserve(4);
-        std::pmr::unordered_set<std::string_view> seen(m_resource);
-        seen.reserve(8);
-
-        while (true)
-        {
-            skip_whitespace();
-            if (*m_curr != '"')
-                error("Expected string key in object");
-            auto key = parse_string();
-            check_duplicate_key(key, seen);
-
-            skip_whitespace();
-            if (*m_curr != ':')
-                error("Expected ':' in object");
-            ++m_curr;
-
-            obj.data().emplace_back(key, Json(nullptr));
-            parse_value_inplace(obj.data().back().second);
-
-            skip_whitespace();
-            if (m_curr >= m_end)
-                error("Unexpected end of object");
-            if (*m_curr == '}')
-            {
-                ++m_curr;
-                return Json(std::move(obj));
-            }
-            if (*m_curr == ',')
-                ++m_curr;
-            else
-                error("Expected ',' or '}' in object");
-        }
+        Json out;
+        parse_object_inplace(out);
+        return out;
     }
 
     void Parser::parse_object_inplace(Json &out)
