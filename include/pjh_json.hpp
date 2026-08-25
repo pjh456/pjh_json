b33ac081f5e33e78b0f6a0bda89fa8d8577beb39
pjh456 <147148383@qq.com>
2026-08-25 16:58:57 +0800

    chore: 添加统一导入头文件

---
+++ include/pjh_json.hpp
@@ -0,0 +1,24 @@
+#ifndef INCLUDE_PJH_JSON_HPP
+#define INCLUDE_PJH_JSON_HPP
+
+/// @file pjh_json.hpp
+/// @brief Umbrella header exposing the complete pjh_json API.
+/// @details Includes the `Json` value type, `Document`, `Parser`, `Writer`,
+///          `Validator`, and `Literal` modules.
+
+#include "pjh_json/array.hpp"
+#include "pjh_json/config.hpp"
+#include "pjh_json/document.hpp"
+#include "pjh_json/error.hpp"
+#include "pjh_json/json.hpp"
+#include "pjh_json/json_constexpr.hpp"
+#include "pjh_json/json_fwd.hpp"
+#include "pjh_json/literal.hpp"
+#include "pjh_json/object.hpp"
+#include "pjh_json/parser.hpp"
+#include "pjh_json/string.hpp"
+#include "pjh_json/utils.hpp"
+#include "pjh_json/validate.hpp"
+#include "pjh_json/writer.hpp"
+
+#endif // INCLUDE_PJH_JSON_HPP
