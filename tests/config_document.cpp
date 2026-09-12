574eaad53feadcbee0be638a60d7c26fc604b1d5
pjh456 <147148383@qq.com>
2026-09-12 07:41:57 +0000

    fix: 补 <ostream> 头修复移除 xsimd 传递包含后的 MSVC 构建

--- tests/config_document.cpp
+++ tests/config_document.cpp
@@ -1,4 +1,5 @@
 #include <doctest/doctest.h>
+#include <ostream>
 #include <cstdio>
 #include <cstring>
 #include <fstream>
--- tests/json_types.cpp
+++ tests/json_types.cpp
@@ -1,4 +1,5 @@
 #include <doctest/doctest.h>
+#include <ostream>
 #include <pjh_json/json.hpp>
 #include <pjh_json/document.hpp>
 #include <pjh_json/parser.hpp>
